6bb0ae6f5b70b636be673350dab33a06492ef8b9
Tamas K. Papp <tkpapp@gmail.com>
2018-07-16 18:39:29 +0200

    Fix test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,16 +6,18 @@ TRAVIS = get(ENV, "TRAVIS", "false") == "true"
 # Test that a text file is opened by Emacs, the default editor. This is
 # accomplished by checking the output of `ps`.
 
+isemacsrunning() = occursin("emacs", read(`ps -e`, String))
+
 if Sys.islinux()
     if TRAVIS
+        @test !isemacsrunning() # check that it is not running accidentally
         testfile = "/tmp/test.txt"
         write(testfile, "test text")
         @info("environment", EDITOR = get(ENV, "EDITOR", "(undefined)"))
-        @test !isfile(sentinelfile)
         @info "opening $(testfile)"
         DefaultApplication.open(testfile)
         @info "emacs should now be running"
-        @test occursin("emacs", read(`ps -e`, String))
+        @test isemacsrunning()
     else
         @warn "Tests are only ran in Travis VM."
     end
