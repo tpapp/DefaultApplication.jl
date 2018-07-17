9a57f3246d168e6efc77945d2aa1e22e82623590
Tamas K. Papp <tkpapp@gmail.com>
2018-07-17 08:15:44 +0200

    test jmacs

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,18 +23,17 @@ end
 
 if Sys.islinux()
     if TRAVIS
-        @test !isrunning("links") # check that it is not running accidentally
-        testfile = "/tmp/test.html"
-        write(testfile, "<html><body><h1>hello world</h1></body><html>")
+        @test !isrunning("jmacs") # check that it is not running accidentally
+        testfile = "/tmp/test.txt"
+        write(testfile, "test text")
         @info("environment",
               XDGMIMETYPE = chomp(read(`xdg-mime query filetype $(testfile)`, String)),
-              XDGMIMEDEFAULT = chomp(read(`xdg-mime query default text/html`, String)))
-        @info "test open" msg = read(`xdg-open $(testfile)`, String)
+              XDGMIMEDEFAULT = chomp(read(`xdg-mime query default text/plain`, String)))
         @info "opening $(testfile)"
         DefaultApplication.open(testfile)
         sleep(5)
-        @info "links should now be running"
-        @test isrunning("links"; details = true)
+        @info "jmacs should now be running"
+        @test isrunning("jmacs"; details = true)
     else
         @warn "Tests are only ran in Travis VM."
     end
