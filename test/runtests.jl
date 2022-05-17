15bebeace12685d6fbd44cef6bec3f8555891356
Tamás K. Papp <tkpapp@gmail.com>
2022-05-17 17:54:15 +0200

    debug enable

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,14 +20,14 @@ end
 
 if Sys.islinux()
     if CI
-        # Test that a text file is opened by jmacs (setup in .travis.yml).
+        # Test that a text file is opened by jmacs (setup in Github Actions).
         @test !isrunning("jmacs") # check that it is not running accidentally
         testfile = "/tmp/test.txt"
         write(testfile, "test text")
         ## uncomment lines below for debug information
-        # @info("environment",
-        #       XDGMIMETYPE = chomp(read(`xdg-mime query filetype $(testfile)`, String)),
-        #       XDGMIMEDEFAULT = chomp(read(`xdg-mime query default text/plain`, String)))
+        @info("environment",
+              XDGMIMETYPE = chomp(read(`xdg-mime query filetype $(testfile)`, String)),
+              XDGMIMEDEFAULT = chomp(read(`xdg-mime query default text/plain`, String)))
         @info "opening $(testfile)"
         DefaultApplication.open(testfile)
         sleep(1)
