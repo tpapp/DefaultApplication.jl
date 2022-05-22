fdd023940a52f298a3d0df934fe40d9ac5e23f5a
Tamás K. Papp <tkpapp@gmail.com>
2022-05-22 15:24:34 +0200

    try waiting

--- src/DefaultApplication.jl
+++ src/DefaultApplication.jl
@@ -27,7 +27,7 @@ function open(filename; wait = false)
         cmd = `powershell.exe -NoProfile -NonInteractive -Command start \"$(relfile)\"`
         run(cmd; wait = wait)
     elseif Sys.islinux() || Sys.isbsd()
-        run(`xdg-open $(filename)`; wait = wait)
+        run(`xdg-open $(filename)`; wait = true)
     elseif Sys.iswindows()
         cmd = get(ENV, "COMSPEC", "cmd.exe")
         run(`$(cmd) /c start $(filename)`; wait = wait)
