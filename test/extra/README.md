1a8ef0ed437394a4c7473ba16f8c54a65597a731
Tamás K. Papp <tkpapp@gmail.com>
2022-05-24 15:36:48 +0200

    new test framework

---
+++ test/extra/README.md
@@ -0,0 +1,1 @@
+These files are used to simulate an application for CI.
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,21 +3,6 @@ import DefaultApplication
 
 CI = get(ENV, "CI", "false") == "true"
 
-function isrunning(program; details = false)
-    processes = read(`ps axco command`, String)
-    if occursin(program, processes)
-        true
-    else
-        if details
-            @info("process $(program) not found in processes")
-            for p in sort(split(processes, "\n"))
-                @info p
-            end
-        end
-        false
-    end
-end
-
 if Sys.islinux()
     if CI
         # Test that a text file is opened by jmacs (setup in Github Actions).
@@ -31,8 +16,8 @@ if Sys.islinux()
         @info "opening $(testfile)"
         DefaultApplication.open(testfile)
         sleep(1)
-        @info "jmacs should now be running"
-        @test isrunning("jmacs"; details = true)
+        @info "test that file was opened running"
+        @test read("/tmp/saved-argument", String) == testfile
     else
         @warn "Tests are only ran in CI."
     end
